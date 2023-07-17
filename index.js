150ccc60e38b655649d381c52fe29378370e93b2
sachin shukla <sachinshukla636@gmail.com>
2023-07-17 18:44:56 +0530

    access value from another file and filter method

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-console.log("Hello World");
-var a=10;
-var b=20;
-console.log(a+b);
+const app = require('./app')
+
+console.log(app)
+
+console.log(app.x)
+console.log(app.y)
+console.log("Sum is ",app.z());
+
+const arr=[2,4,7,1,3,8,3];
+
+let result=arr.filter((item)=>{
+    return item==3;
+})
+console.log(result);
